606b9cdee528f892fdac6df43d177f4f7d8fe84a
Beebarh.jy <153818039+Barbexa@users.noreply.github.com>
2025-05-01 00:38:32 +0100

    Add files via upload

---
+++ Enactus3/custom.js
@@ -0,0 +1,39 @@
+
+//Initialize AOS 
+AOS.init({
+    duration: 1000,
+    once: true
+})
+
+//counter animation
+const counters = document.querySelectorAll('.counter');
+let counterStarted = false;
+
+const startCounters = () => {
+    counters.forEach(counter => {
+        counter.innerText = '0';
+        const updateCounter = () => {
+            const target = +counter.getAttribute('data-target');
+            const count = +counter.innerText;
+            const increment = target / 100;
+
+            if (count < target) {
+                counter.innerText = Math.ceil(count + increment);
+                setTimeout(updateCounter, 20);
+            } else {
+                counter.innerText = target
+            };
+        };
+        updateCounter();
+    });
+};
+
+window.addEventListener('scroll', () => {
+    const counterSection = document.querySelector('.counter-section');
+    const sectionTop = counterSection.offsetTop;
+
+    if (!counterStarted && window.screenY + window.innerHeight >= sectionTop) {
+        startCounters();
+        counterStarted = true;
+    }
+})
